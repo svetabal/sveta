a82aa31be89a7cc7cbefb1fa3961544dc1f39cd0
svetabal <y98wk5nmg7@privaterelay.appleid.com>
2026-05-25 19:40:40 +0300

    iter 2: test for nonexistent item

--- warehouse/test_warehouse.py
+++ warehouse/test_warehouse.py
@@ -4,3 +4,11 @@ def test_add_item():
     w = Warehouse()
     w.add_item("apple", 10)
     assert w.get_quantity("apple") == 10
+
+def test_get_nonexistent_item():
+    w = Warehouse()
+    try:
+        w.get_quantity("banana")
+        assert False
+    except KeyError:
+        assert True
